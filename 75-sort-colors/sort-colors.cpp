24adfc8c913d4387e629db1b93fe89addd2dbe42
procoder-divyanshv <divyanshv443@gmail.com>
2025-12-04 23:30:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (12.00%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,17 +1,64 @@
 class Solution {
 public:
-    vector<int> sortColors(vector<int>& nums) {
-        int mini;
-        int n= nums.size();
-        for(int i=0;i<n-1;i++){
-            mini=i;
-            for(int j=i+1;j<n;j++){
-                if(nums[mini]>nums[j]){
-                    swap(nums[mini],nums[j]);
-                    // mini=j;
-                }
+    vector<int> merge(vector<int> a,vector<int> b){
+        vector<int>c(a.size()+b.size());
+        int i=0;
+        int j=0;
+        int idx=0;
+        while(i<a.size() && j<b.size()){
+            if(a[i]<=b[j]){
+                c[idx]=a[i];
+                i++;
+                idx++;
+
             }
+            else{
+                c[idx]=b[j];
+                j++;
+                idx++;
+
+            }
+
         }
-        return nums;
+        while(i<a.size()){
+            c[idx]=a[i];
+            i++;
+            idx++;
+
+        }
+        while(j<b.size()){
+            c[idx]=b[j];
+            j++;
+            idx++;
+
+        }
+        return c;
+    }
+    void sortColors(vector<int>& nums) {
+        //sorting is not allowed this time.
+        vector<int>a;
+        vector<int>b;
+        vector<int>c;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                a.push_back(nums[i]);
+            }
+            else if(nums[i]==1){
+                b.push_back(nums[i]);
+
+            }
+            else{
+                c.push_back(nums[i]);
+            }
+
+        }
+        vector<int>d=merge(a,b);
+        vector<int>e= merge(d,c);
+        // return e;
+        nums=e;
+
+
+
+
     }
 };
