64c5006c6c8b2e0ea6feb262b0e489e7929ae8ef
procoder-divyanshv <divyanshv443@gmail.com>
2025-12-07 00:27:23 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (12.04%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,61 +1,82 @@
 class Solution {
 public:
-    vector<int> merge(vector<int> a,vector<int> b){
-        vector<int>c(a.size()+b.size());
-        int i=0;
-        int j=0;
-        int idx=0;
-        while(i<a.size() && j<b.size()){
-            if(a[i]<=b[j]){
-                c[idx]=a[i];
-                i++;
-                idx++;
+    //vector<int> merge(vector<int> a,vector<int> b){
+    //     vector<int>c(a.size()+b.size());
+    //     int i=0;
+    //     int j=0;
+    //     int idx=0;
+    //     while(i<a.size() && j<b.size()){
+    //         if(a[i]<=b[j]){
+    //             c[idx]=a[i];
+    //             i++;
+    //             idx++;
 
-            }
-            else{
-                c[idx]=b[j];
-                j++;
-                idx++;
+    //         }
+    //         else{
+    //             c[idx]=b[j];
+    //             j++;
+    //             idx++;
 
-            }
+    //         }
 
-        }
-        while(i<a.size()){
-            c[idx]=a[i];
-            i++;
-            idx++;
+    //     }
+    //     while(i<a.size()){
+    //         c[idx]=a[i];
+    //         i++;
+    //         idx++;
 
-        }
-        while(j<b.size()){
-            c[idx]=b[j];
-            j++;
-            idx++;
+    //     }
+    //     while(j<b.size()){
+    //         c[idx]=b[j];
+    //         j++;
+    //         idx++;
 
-        }
-        return c;
-    }
+    //     }
+    //     return c;
+    // }
     void sortColors(vector<int>& nums) {
-        //sorting is not allowed this time.
-        vector<int>a;
-        vector<int>b;
-        vector<int>c;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                a.push_back(nums[i]);
-            }
-            else if(nums[i]==1){
-                b.push_back(nums[i]);
+    //     //sorting is not allowed this time.
+    //     vector<int>a;
+    //     vector<int>b;
+    //     vector<int>c;
+    //     for(int i=0;i<nums.size();i++){
+    //         if(nums[i]==0){
+    //             a.push_back(nums[i]);
+    //         }
+    //         else if(nums[i]==1){
+    //             b.push_back(nums[i]);
 
-            }
-            else{
-                c.push_back(nums[i]);
-            }
+    //         }
+    //         else{
+    //             c.push_back(nums[i]);
+    //         }
+
+    //     }
+    //     vector<int>d=merge(a,b);
+    //     vector<int>e= merge(d,c);
+    //     // return e;
+    //     nums=e;
+    int low=0;
+    int mid=0;
+    int high=nums.size()-1;
+    while(mid<=high){
+        if(nums[mid]==0){
+            swap(nums[low],nums[mid]);
+            low++;
+            mid++;
 
         }
-        vector<int>d=merge(a,b);
-        vector<int>e= merge(d,c);
-        // return e;
-        nums=e;
+        else if(nums[mid]==1){
+            //best case;
+            mid++;
+        }
+        else if(nums[mid]==2){
+            swap(nums[mid],nums[high]);
+            high--;
+
+        }
+    }
+    // return nums;
 
 
 
