34ba4e5c5fb4a66cfe0b96675ce41effe4bc0f6f
procoder-divyanshv <divyanshv443@gmail.com>
2025-12-11 14:11:52 +0530

    Time: 6 ms (72.86%) | Memory: 11.3 MB (82.21%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -11,7 +11,7 @@ public:
     ListNode *detectCycle(ListNode *head) {
         ListNode* slow=head;
         ListNode* fast=head;
-        while(fast != NULL && fast->next != NULL){
+        while(slow!= NULL && slow->next!=NULL && fast != NULL && fast->next != NULL){
             slow= slow->next;
             fast = fast->next->next;
             if(slow == fast){
