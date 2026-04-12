ac2e261f1ffb3e490d5fa7b2a25afe5f7be12238
procoder-divyanshv <divyanshv443@gmail.com>
2026-04-13 00:13:25 +0530

    Time: 614 ms (5.05%) | Memory: 190.6 MB (5.39%) - LeetSync

---
+++ 77-combinations/combinations.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    set<vector<int>> subsets_set;
+    vector<vector<int>>subsets;
+    void generate_subset(vector<int> & nums,int n,int k){
+        int range = (1<<n);
+        for(int i=0;i<range;i++){
+            vector<int>subset;
+            if(__builtin_popcount(i)==k){
+                    for(int j=0;j<n;j++){
+                        if((i &(1<<j)) != 0){
+                        
+                            subset.push_back(nums[j]);
+                        
+                        }
+                    }
+                    subsets_set.insert(subset);
+            }
+            
+        }
+        for(auto i : subsets_set){
+            subsets.push_back(i);
+        }
+
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>nums;
+        for(int i=1;i<=n;i++){
+            nums.push_back(i);
+        }
+
+        generate_subset(nums, n,k);
+        return subsets;
+
+        
+    }
+};
